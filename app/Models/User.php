8947ec16c5b976766274323056f1fccc1bc85ce7
JawadRustom <jawad.ru1@gmail.com>
2025-06-25 06:02:02 +0300

    add filament and favicon and brandlogo

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,16 +3,24 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasApiTokens,HasFactory, Notifiable;
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return $this->email === 'admin@admin.com' && $this->hasVerifiedEmail();
+    }
+
 
     /**
      * The attributes that are mass assignable.
---
+++ resources/views/brand-logo.blade.php
@@ -0,0 +1,10 @@
+<div>
+    <a href="{{ config('app.url') }}">
+        <img
+            alt="AGF tutoring"
+            loading="lazy"
+            src="{{ asset('brand-logo.svg') }}"
+            style="height: 2rem;" class="fi-logo inline-flex"
+        />
+    </a>
+</div>
